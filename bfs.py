75c6be1cabc6c4899081d2c923f2fc3374c6b484
Helia Taromi <heliataromi@gmail.com>
2024-11-01 22:49:25 +0330

    Add BFS for a given tree

---
+++ bfs.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+from tree import Tree
+
+def bfs(tree):
+    """Perform a breadth-first search on the given tree."""
+    root = tree.get_root()
+    if root is None:
+        return []
+
+    queue = deque([root])
+    visited = {root}
+    traverse = []
+
+    while queue:
+        current = queue.popleft()
+        traverse.append(current)
+        for child in current.children:
+            if child not in visited:
+                queue.append(child)
+                visited.add(child)
+
+    return traverse
